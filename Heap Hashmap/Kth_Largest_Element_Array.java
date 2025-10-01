697fcdbbde0eeba9be1e40a6c5b0a5f0ab14f9bf
Yachna Raghav <142567363+Yachna63893@users.noreply.github.com>
2025-10-01 23:40:36 +0530

    Add files via upload

---
+++ Heap Hashmap/Kth_Largest_Element_Array.java
@@ -0,0 +1,21 @@
+package Heap_Hashmap;
+
+import java.util.PriorityQueue;
+
+public class Kth_Largest_Element_Array {
+    public static void main(String[] args) {
+        int[] arr = {3,2,3,1,2,4,5,5,6};
+        int k = 4;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<k; i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k; i<arr.length; i++){
+            if(pq.peek()<arr[i]){
+                pq.remove(); //pq.poll();
+                pq.add(arr[i]);
+            }
+            System.out.println(pq.peek());
+        }
+    }
+}
---
+++ Heap Hashmap/PriorityQueue_Demo.java
@@ -0,0 +1,21 @@
+package Heap_Hashmap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+ 
+public class PriorityQueue_Demo {
+    public static void main(String[] args) {
+        // PriorityQueue<Integer> hp = new PriorityQueue<>();//minheap
+        PriorityQueue<Integer> hp = new PriorityQueue<>(Collections.reverseOrder());//maxheap
+        hp.add(10);
+        hp.add(20);
+        hp.add(30);
+        hp.add(1);
+        hp.add(5);
+        hp.add(7);
+        hp.add(3);
+        hp.add(2);
+        hp.add(-4);
+        System.out.println(hp);
+    }
+}
