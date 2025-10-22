79058e0c6102ab7b9ffb4bf43607858633cbfcb5
Yachna Raghav <142567363+Yachna63893@users.noreply.github.com>
2025-10-22 23:04:15 +0530

    Add files via upload

---
+++ Tree/BinaryTree_Client.java
@@ -0,0 +1,12 @@
+package Tree;
+public class BinaryTree_Client{
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();     
+        bt.Display();
+        // 10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false
+        System.out.println(bt.max());
+        System.out.println(bt.find(65));
+        System.out.println(bt.find(60));
+
+    }
+}
